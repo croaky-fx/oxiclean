009900a1adf00eb1280264b9773bbd8daff407a6
croaky-fx <mohamedmohed3908@gmail.com>
2026-03-27 23:40:33 +0200

    remove temp file

--- _write_clean.py
+++
@@ -1,9 +0,0 @@
-
-import os
-path = "src/clean.rs"
-# Read the file from a separate text file
-with open("_clean_content.txt", "r") as c:
-    content = c.read()
-with open(path, "w") as out:
-    out.write(content)
-print("Written: " + path)
